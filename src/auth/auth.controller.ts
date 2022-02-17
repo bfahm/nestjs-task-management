04a96ac8bd3c263b0090b055d7aac4aceffa3f49
bola <bola_fah.97@hotmail.com>
2022-02-17 12:59:39 +0200

    Added a test endpoint for debugging and logging

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { AuthResponseDto } from './dto/auth-response.dto';
 
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger(AuthController.name);
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
@@ -15,5 +16,15 @@ export class AuthController {
   @Post('signin')
   signIn(@Body() request: AuthCredentialDto): Promise<AuthResponseDto> {
     return this.authService.signIn(request);
+  }
+
+  @Get()
+  getError(): string {
+    try {
+      throw new Error('Error!');
+    } catch (error) {
+      this.logger.error('AN ERROR OCCURED', error.stack);
+    }
+    return 'This is an error';
   }
 }
